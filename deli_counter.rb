26d1fd3f991287e55c2931d8b38e0b232cc7b5f7
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-05 16:41:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,37 @@
 katz_deli = []
+$counter = 0
 
 def line(katz_deli)
-  if katz_deli.any?
-    deli_line = "The line is currently:"
-    counter = 0
-    katz_deli.each do |person|
-      counter += 1
-      deli_line += " #{counter}. #{person}"
-    end
-  puts deli_line
-  else
+  if katz_deli.empty?
     puts "The line is currently empty."
+  else
+    deli_line = katz_deli.map.with_index(1) {|customer, position| "#{position}. " "#{customer}" }
+    puts "The line is currently: " + deli_line.join(" ")
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length()} in line."
+# def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.length()} in line."
+# end
+
+def take_a_number(katz_deli)
+  katz_deli << katz_deli.length() +1 
+  puts katz_deli
 end
+
 
 def now_serving(katz_deli)
-  if katz_deli.any?
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-  else
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
+
+take_a_number(katz_deli)
+take_a_number(katz_deli)
+take_a_number(katz_deli)
+take_a_number(katz_deli)
+take_a_number(katz_deli)
+take_a_number(katz_deli)
